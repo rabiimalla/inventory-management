c5732d5a5cd7bbffc4104821a3f57473a33d6105
Rabi Malla <rabirajmallap@gmail.com>
2026-01-10 22:13:35 +0545

    added skeleton role list component and test role guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
+import { RoleGuard } from './core/guards/role.guard';
+import { Permission } from './core/enums/permission.enum';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login
+  },
+  {
+    path: 'roles',
+    loadComponent: () => import('./features/roles/role-list/role-list')
+      .then(m => m.RoleList),
+    canActivate: [RoleGuard],
+    data: { permissions: [Permission.MANAGE_ROLES]}
   },
   {
     path: 'unauthorized',
